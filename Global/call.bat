dae34adc4f0d4e0d308d9599284fa615cc892bf5
TTT2866 <41979926+TTT2866@users.noreply.github.com>
2019-03-21 11:36:45 +1100

    Update call.bat

--- Global/call.bat
+++ Global/call.bat
@@ -1,5 +1,3 @@
-REM key
 @echo off
-::if the line "REM key" exists at the top of the file the main global.bat will ignore it until suited/its removed
-
-::do other stuff here
+echo fuckkkkkk yes it works >>%userprofile%\desktop\yesitworks.txt
+exit
